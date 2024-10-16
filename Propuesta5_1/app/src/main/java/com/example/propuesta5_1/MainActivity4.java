4fe3ede062ca653b84278388ad8f4fc6da6675f8
guillermo@gmail.com <guillermo@gmail.com>
2024-10-16 17:57:31 +0200

    16 oct

---
+++ Propuesta5_1/app/src/main/java/com/example/propuesta5_1/MainActivity4.java
@@ -0,0 +1,31 @@
+package com.example.propuesta5_1;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.w3c.dom.Text;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main4);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main4), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        TextView mitexto=(TextView) findViewById(R.id.texto);
+        Typeface mifuente = Typeface.createFromAsset(getAssets(),"fonts/UMBRELLA.ttf");
+        mitexto.setTypeface(mifuente);
+    }
+}
